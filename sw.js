421223e0f0351827c8c01e4b0341fc3df399a1fd
jhim24 <85859168+jhim24@users.noreply.github.com>
2026-05-12 15:33:54 +0300

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,39 @@
+const CACHE_NAME = "papprito-cache-v1";
+
+const urlsToCache = [
+  "/",
+  "/login.html",
+  "/index.html",
+  "/employee-portal.html",
+  "/logo.png"
+];
+
+self.addEventListener("install", event => {
+
+  event.waitUntil(
+
+    caches.open(CACHE_NAME)
+    .then(cache => {
+
+      return cache.addAll(urlsToCache);
+
+    })
+
+  );
+
+});
+
+self.addEventListener("fetch", event => {
+
+  event.respondWith(
+
+    caches.match(event.request)
+    .then(response => {
+
+      return response || fetch(event.request);
+
+    })
+
+  );
+
+});
